400c0fe92fed8f58daa0bbb51e6181bd4c1c8d44
Gera Volobuev <fromgayva@gmail.com>
2017-01-24 07:28:51 +0000

    add tumblr api

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-// Full page scroll.
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-        slidesNavigation: true,
-    });
-});
-
 // Humburger icon.
 $(document).ready(function(){
 	$('#nav-icon1').click(function(){
